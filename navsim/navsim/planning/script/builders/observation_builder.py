838c9b4cbc0472ea2e6bb9a1d140cc3d2245b567
aarchiiive <archiiive99@gmail.com>
2025-10-30 19:46:40 +0900

    Add evaluation scripts, training scripts, and visualization tutorial for NAVSIM
    - Created multiple evaluation scripts for different agents (e.g., ego MLP, human, transfuser) to assess performance metrics.
    - Added training scripts for ego MLP and transfuser agents.
    - Introduced a requirements.txt file to manage dependencies for the NAVSIM project.
    - Implemented a setup.py for package installation.
    - Developed a comprehensive visualization tutorial in Jupyter Notebook to demonstrate plotting capabilities in NAVSIM.

---
+++ navsim/navsim/planning/script/builders/observation_builder.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+from typing import cast
+
+from hydra.utils import instantiate
+from nuplan.planning.scenario_builder.abstract_scenario import AbstractScenario
+from nuplan.planning.simulation.observation.abstract_observation import AbstractObservation
+from omegaconf import DictConfig
+
+
+def build_observations(observation_cfg: DictConfig, scenario: AbstractScenario) -> AbstractObservation:
+    """
+    Instantiate observations
+    :param observation_cfg: config of a planner
+    :param scenario: scenario
+    :return AbstractObservation
+    """
+    observation = cast(AbstractObservation, instantiate(observation_cfg, scenario=scenario))
+    return observation
